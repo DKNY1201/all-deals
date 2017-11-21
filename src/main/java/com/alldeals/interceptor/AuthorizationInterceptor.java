a1c8660d29c8781aba60ac6c60458180af76a178
Quy Tran <quytran288@gmail.com>
2017-11-21 00:46:48 -0600

    Fix error when check admin role for accessing retrict urls

--- src/main/java/com/alldeals/interceptor/AuthorizationInterceptor.java
+++ src/main/java/com/alldeals/interceptor/AuthorizationInterceptor.java
@@ -5,19 +5,22 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.alldeals.exception.AccessDeniedException;
 import com.alldeals.utils.UrlUtils;
-import org.apache.log4j.Logger;
-import org.springframework.web.servlet.AsyncHandlerInterceptor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.security.Principal;
 
 public class AuthorizationInterceptor implements HandlerInterceptor {
 
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-		if(!request.isUserInRole("ADMIN")) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		boolean hasAdminRole = authentication.getAuthorities().stream()
+				.anyMatch(r -> r.getAuthority().equals("ADMIN"));
+		if(!hasAdminRole) {
 			throw new AccessDeniedException(UrlUtils.getFullURL(request));
 		}
+		
 		return true;
 	}
 
